9b656d4c0d2f854f933ede76b4aa9e59934226e3
Farhana Aktar <farhanaaktar@live.co.uk>
2024-11-13 11:38:14 +0000

    server working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,34 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const Project = require('./projects-model')
+
+const router = express.Router()
+
+
+router.get('/', (req,res) => {
+
+})
+
+router.get('/:id', (req,res) => {
+    
+})
+
+router.post('/:id', (req,res) => {
+    
+})
+
+router.put('/', (req,res) => {
+    
+})
+
+router.delete('/:id', (req,res) => {
+    
+})
+
+router.get('/:id/actions', (req,res) => {
+    
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router')
+
+server.use(express.json())
+server.use('/api/projects', projectsRouter)
+
+
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Lets build this project`)
+})
+module.exports = server;
+
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
-
-module.exports = server;
