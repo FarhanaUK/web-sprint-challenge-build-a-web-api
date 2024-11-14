c859b05ce1cc975de7eb097bbc954b3430b874e8
Farhana Aktar <farhanaaktar@live.co.uk>
2024-11-14 11:25:44 +0000

    delete and getByAction passed

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,49 +1,69 @@
 // Write your "project" router here!
-const express = require('express')
+const express = require("express");
 
-const Project = require('./projects-model')
-const Actions = require('../actions/actions-model')
-const {validateProjectData, errorHandler, validateById,} = require('./projects-middleware')
+const Project = require("./projects-model");
+const Actions = require("../actions/actions-model");
+const {
+  validateProjectData,
+  errorHandler,
+  validateById,
+} = require("./projects-middleware");
 
-const router = express.Router()
+const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const project = await Project.get();
+  if (!project || project.length === 0) {
+    res.send([]);
+  } else {
+    res.json(project);
+  }
+});
 
-router.get('/', async(req,res) => {
-const project = await Project.get()
-if(!project || project.length === 0){
-res.send([])
-}else{
-    res.json(project)
-}
-})
+router.get("/:id", validateById, (req, res) => {
+  res.json(req.body);
+});
 
-router.get('/:id', validateById,(req,res) => {
- res.json(req.body)
-})
-
-router.post('/', validateProjectData,(req, res, next) => {
-    Project.insert({
-        name: req.body.name,
-        description: req.body.description,
-        completed: req.body.completed || false,
+router.post("/", validateProjectData, (req, res, next) => {
+  Project.insert({
+    name: req.body.name,
+    description: req.body.description,
+    completed: req.body.completed || false,
+  })
+    .then((newProject) => {
+      res.status(201).json(newProject);
     })
-    .then(newProject => {
-     res.status(201).json(newProject)
+    .catch(next);
+});
+
+router.put("/:id", validateProjectData, validateById, (req, res, next) => {
+  Project.update(req.params.id, req.body)
+
+    .then(() => {
+      return Project.get(req.params.id);
     })
-    .catch(next)
-})
+    .then((project) => {
+      res.json(project);
+    })
+    .catch(next);
+});
 
-router.put('/', (req,res) => {
-    
-})
+router.delete("/:id", validateById, async (req, res, next) => {
+  try {
+    await Project.remove(req.params.id);
+    res.json(req.project);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.delete('/:id', (req,res) => {
-    
-})
+router.get("/:id/actions", validateById, async (req, res, next) => {
+  try {
+    const result = await Project.getProjectActions(req.params.id);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:id/actions', (req,res) => {
-    
-})
-
-
-module.exports = router
+module.exports = router;
